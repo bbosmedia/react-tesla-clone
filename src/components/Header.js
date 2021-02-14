1dd74c1afcfe61f982b5cb240fa18e164d49f108
bbosmedia <bbosmedia@users.noreply.github.com>
2021-02-14 13:13:00 +0500

    Header Section is added

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Header.css"
+import TeslaLogo from "../assets/teslaLogoSmall.svg"
+import MenuIcon from '@material-ui/icons/Menu';
+
+const Header = () => {
+    return (
+        <header className="header">
+           <div className="header__logo">
+               <img src={TeslaLogo} alt=""/>
+            </div> 
+            <div className="header__center">
+                <p>Model S</p>
+                <p>Model 3</p>
+                <p>Model X</p>
+                <p>Model Y</p>
+                <p>Solar Roof</p>
+                <p>Solar Panel</p>
+            </div>
+            <div className="header__right">
+                <p>Shop</p>
+                <p>Tesla Account</p>
+                <p><MenuIcon /></p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
